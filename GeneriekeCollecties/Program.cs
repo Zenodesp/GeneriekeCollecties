202a524bb408ca869945fdf5d8b2746ca5ac1b1f
Zenodesp <Zeno.despiegelaere@student.ehb.be>
2023-09-26 13:32:16 +0200

    Add project files.

---
+++ GeneriekeCollecties/Program.cs
@@ -0,0 +1,49 @@
+﻿using GeneriekeCollecties;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Pile<int> p1 = new Pile<int>();
+        char keuze = '0';
+        do { 
+
+            Console.WriteLine("Select your option:");
+            Console.WriteLine("1 Add");
+            Console.WriteLine("2 Remove");
+            Console.WriteLine("3 Clear");
+            Console.WriteLine("4 Print");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Enter choice now: ");
+            keuze = (char)Console.Read();
+
+            switch (keuze)
+            {
+                case '0':
+                    break;
+                case '1':
+                    Console.WriteLine("Enter your input:");
+                    p1.AddTo(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                case '2':
+                    if (p1.isLeeg())
+                    {
+
+                        Console.WriteLine("This row is empty.");
+
+                    }
+                    else
+                        Console.WriteLine("The number you removed was: " + p1.RemoveFrom());
+                    break;
+                case '3':
+                    p1.Clear();
+                    Console.WriteLine("The row is cleared.");
+                    break;
+                case '4':
+                    Console.WriteLine("All of the objects in your row:");
+                    Console.Write(p1.ToString());
+                    break;
+            }
+
+        } while (keuze != '0');
+    }
+}
---
+++ GeneriekeCollecties/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneriekeCollecties
+{
+    internal class Pile<T>
+    {
+        List<T> container;
+
+        public void AddTo(T i)
+        {
+            container.Add(i);
+        }
+
+        public bool isLeeg()
+        {
+            return container.Count == 0;
+        }
+
+        public T RemoveFrom()
+        {
+            T even = container[0];
+            container.RemoveAt(0);
+            return even;
+        }
+
+        public void Clear()
+        {
+            container.Clear();
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (T i in container)
+            {
+                s += i.ToString() + "\n";
+            }
+            return s;
+        }
+
+
+    }
+}
